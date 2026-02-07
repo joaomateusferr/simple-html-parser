bec3340a29641d3c29e44c4178cc94e235d51f13
Joao Ferreira <joaomateusferr@gmail.com>
2026-02-07 13:36:00 -0300

    Add parse methods

--- html_parser/Parser.py
+++ html_parser/Parser.py
@@ -1,19 +1,32 @@
 from lxml import html
+from bs4 import BeautifulSoup
 
 class Parser:
 
+    title: str | None
+    content: str | None
+
     def __init__(self, html_content):
-        self.html_content = html_content
+
+        self.title = self.parse_title(html_content)
+
+        body = self.parse_body(html_content)
+
+        if body :
+            self.content = self.parse_content(body)
+        else :
+            self.content = None
 
     def __str__(self):
-        return f"html_content : {self.html_content}"
+        return f"title : {self.title}, content : {self.content}"
 
     def __repr__(self):
-        return f"Parser(html_content='{self.html_content}')"
+        return f"Parser(title='{self.title},content='{self.content}')"
 
-    def get_title(self: str) -> str | None:
+    @staticmethod
+    def parse_title(html_content : str) -> str | None:
 
-        tree = html.fromstring(self.html_content)
+        tree = html.fromstring(html_content)
         query = "//head/title/text()"
         entries = tree.xpath(query)
 
@@ -21,3 +34,32 @@ class Parser:
             return entries[0].strip()
 
         return None
+
+    @staticmethod
+    def parse_body(html_content : str) -> str | None:
+
+        result = None
+
+        soup = BeautifulSoup(html_content, 'html.parser')
+        body = soup.find('body')
+
+        if body:
+            result = "".join(str(child) for child in body.contents)
+
+        return result
+
+    @staticmethod
+    def parse_content(body: str) -> str:
+
+        soup = BeautifulSoup(body, "html.parser")
+
+        for script_or_style in soup(["script", "style"]):
+            script_or_style.decompose()
+
+        return " ".join(soup.get_text().split())
+
+    def get_title(self) -> str | None:
+        return self.title
+
+    def get_content(self) -> str | None:
+        return self.content
--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from html_parser.Parser import Parser
 
 Page = '<!DOCTYPE html><html><head><title>Título da Página</title></head><body><h1>Olá Mundo</h1><p>Este é o corpo da página em uma única linha.</p></body></html>'
 Parser = Parser(Page)
-print(Parser.get_title())
+print(Parser)
