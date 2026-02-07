46ed41229fec69b4f728e3d929243674a705d1c5
Joao Ferreira <joaomateusferr@gmail.com>
2026-02-07 13:46:56 -0300

    Add tests

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from html_parser.Parser import Parser
+from pathlib import Path
 
-Page = '<!DOCTYPE html><html><head><title>Título da Página</title></head><body><h1>Olá Mundo</h1><p>Este é o corpo da página em uma única linha.</p></body></html>'
+#Page = '<!DOCTYPE html><html><head><title>Título da Página</title></head><body><h1>Olá Mundo</h1><p>Este é o corpo da página em uma única linha.</p></body></html>'
+Page = Path('/home/john/Desktop/site.html').read_text(encoding='utf-8')
 Parser = Parser(Page)
 print(Parser)
